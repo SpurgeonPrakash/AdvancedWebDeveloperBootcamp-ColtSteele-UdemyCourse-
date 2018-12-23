43c1b98fda76c4bb3ed4ec1352283eef11227c37
silvicardo <silvicardo@gmail.com>
2018-12-23 13:38:25 +0100

    reduce exercises

--- Advanced Array Methods/reduce/main.js
+++ Advanced Array Methods/reduce/main.js
@@ -110,14 +110,17 @@ console.log('ESERCIZI REDUCE');
 Write a function called extractValue which accepts an array of objects and a key and returns a new array with the value of each object at the key.
 
 Examples:
-    var arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]
-    extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
 */
 
-function extractValue(arr, key){
-
+function extractValue(arr, key) {
+  return arr.reduce(function (accumulator, nextValue, index, fullArray) {
+    accumulator.push(nextValue[key]);
+    return accumulator;
+  }, []);
 }
 
+var arr = [{ name: 'Elie' }, { name: 'Tim' }, { name: 'Matt' }, { name: 'Colt' }];
+console.log(extractValue(arr, 'name')); // ['Elie', 'Tim', 'Matt', 'Colt']
 
 /*
 Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
@@ -130,10 +133,46 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str){
+function vowelCount(str) {
+  var vowels = 'aeiou';
+  return str.toLowerCase().split('').reduce(function (accumulator, nextValue, index, fullWord) {
 
+    if (!vowels.includes(nextValue)) {
+      return accumulator;
+    }
+
+    if (nextValue in accumulator) {
+      accumulator[nextValue]++;
+    } else {
+      accumulator[nextValue] = 1;
+    }
+
+    return accumulator;
+  }, {});
 }
 
+/*
+NOTA CORREZIONE: è possibile invertire la condizione includes per
+scrivere return solo una volta
+
+function vowelCount(str) {
+  var vowels = 'aeiou';
+  return str.toLowerCase().split('').reduce(function (accumulator, nextValue, index, fullWord) {
+
+    if (vowels.includes(nextValue)) {
+      if (nextValue in accumulator) {
+        accumulator[nextValue]++;
+      } else {
+        accumulator[nextValue] = 1;
+      }
+    }
+    return accumulator;
+  }, {});
+}
+*/
+
+console.log(vowelCount('Elie'));
+console.log(vowelCount('I Am awesome and so are you'));
 /*
 Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
 
@@ -149,10 +188,29 @@ Examples:
        ]
 */
 
-function addKeyAndValue(arr, key, value){
-
+function addKeyAndValue(arr, key, value) {
+  return arr.reduce(function (accumulator, nextVal, index, fullArr) {
+    accumulator[index] = nextVal;
+    accumulator[index][key] = value;
+    return accumulator;
+  }, []);
 }
 
+/*
+NOTA CORREZIONE: E' possibile dare l'array iniziale come accumulator
+
+function addKeyAndValue(arr, key, value) {
+  return arr.reduce(function (accumulator, nextVal, index, fullArr) {
+    accumulator[index][key] = value;
+    return accumulator;
+  }, arr);
+}
+
+*/
+
+var arr = [{ name: 'Elie' }, { name: 'Tim' }, { name: 'Matt' }, { name: 'Colt' }];
+
+console.log(addKeyAndValue(arr, 'title', 'Instructor'));
 
 /*
 Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray.
@@ -176,6 +234,30 @@ Examples:
     partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
 */
 
-function partition(arr, callback){
+function partition(arr, callback) {
+  return arr.reduce(function (accumulator, nextVal) {
+    if (callback(nextVal)) {
+      accumulator[0].push(nextVal);
+    } else {
+      accumulator[1].push(nextVal);
+    }
 
+    return accumulator;
+  }, [[], []]);
 }
+
+function isLongerThanThreeCharacters(val) {
+  return val.length > 3;
+}
+
+var names = ['Elie', 'Colt', 'Tim', 'Matt'];
+
+console.log(partition(names, isLongerThanThreeCharacters)); // [['Elie', 'Colt', 'Matt'], ['Tim']]
+
+function isEven(val) {
+  return val % 2 === 0;
+}
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+console.log(partition(arr, isEven)); // [[2,4,6,8], [1,3,5,7]];
