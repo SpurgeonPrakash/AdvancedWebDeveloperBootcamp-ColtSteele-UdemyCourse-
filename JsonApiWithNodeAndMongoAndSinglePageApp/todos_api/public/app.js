a72a32ec414ba3ec623096edefa2ec8b0901df75
silvicardo <silvicardo@gmail.com>
2019-01-13 16:40:16 +0100

    main route ajax call and todos li generation function

--- JsonApiWithNodeAndMongoAndSinglePageApp/todos_api/public/app.js
+++ JsonApiWithNodeAndMongoAndSinglePageApp/todos_api/public/app.js
@@ -2,8 +2,37 @@ console.log('app.js connected');
 
 $(document).ready(function () {
 
-  $('body').click(function () {
-    alert('cliccato, jquery funziona');
+  //a questo punto siamo nella directory principale
+  //come url della richiesta get specifichiamo solo
+  //l'endpoint.
+  //usiamo il metodo getJSON per abbreviare
+  $.getJSON('/api/todos')
+  .then(addTodos)
+  .catch(function (error) {
+    console.log(error);
   });
 
 });
+
+function addTodos(todos) {
+  console.log(todos);
+  todos.forEach(function (todoFromApi) {
+    console.log(todoFromApi.name);
+
+    //creiamo il tag li
+    //racchiudiamo il tag in $() per
+    //poter applicare i metodi di jQuery
+    //come ad esempio addClass per stilarlo
+    var $newTodoLi = $('<li>' + todoFromApi.name + '</li>').addClass('task');
+
+    //Se la proprietà completed dell'oggetto 'todo'
+    //dall'api è true aggiungiamo la classe 'done'
+    if (todoFromApi.completed) {
+      newTodo.addClass('done');
+    }
+
+    //e lo aggiungiamo alla lista
+    $('ul.list').append($newTodoLi);
+
+  });
+}
