efc3accb1aa3a8909e50359f6d11bdc487653595
silvicardo <silvicardo@gmail.com>
2019-02-22 10:53:52 +0100

    es2015 rest, spread, objects

--- React/State/update-complex-state/src/App.js
+++ React/State/update-complex-state/src/App.js
@@ -21,7 +21,57 @@ class App extends Component {
         }
       ]
     };
+
+    {/* MODIFICARE CORRETTAMENTE this.state */}
+
+    {/* MODO 1: SLICE, OBJECT.ASSIGN, SPLICE */}
+
+    { /*
+        setTimeout(() => {
+          this.state.instructors.forEach ((inst) => {
+            console.log('this.state inizio: ' + inst.hobbies);
+          });
+
+
+          const instIndex = Math.floor(Math.random() * this.state.instructors.length);
+
+          const hobbyIndex = Math.floor(Math.random() * this.state.instructors[instIndex].length);
+
+          console.log('indice modifica ' + instIndex);
+
+          //Duplichiamo l'array di oggetti intructors
+          const instructors = this.state.instructors.slice();
+
+          //Facciamo una copia indipendente, che non faccia alcun
+          //riferimento all'oggetto originale
+          //Object.assign(nuovoOggettoVuoto, oggettoDaCopiare)
+          instructors[instIndex] = Object.assign({}, instructors[instIndex]);
+
+          //per togliere la referenza anche all'array hobbies
+          //lo sleghiamo con un duplicato
+
+           instructors[instIndex].hobbies = instructors[instIndex].hobbies.slice();
+
+           //Ora non ci sono legami con this.setState, possiamo
+          //modificare direttamente l'array hobbies all'istruttore scelto
+
+          instructors[instIndex].hobbies.splice(hobbyIndex, 1);
+
+          this.state.instructors.forEach ((inst) => {
+            console.log('this.state dopo modifica copia: ' + inst.hobbies);
+          });
+
+          //Procediamo con setState
+          this.setState({instructors});
+
+          this.state.instructors.forEach ((inst) => {
+            console.log('this.state dopo setState: ' + inst.hobbies);
+          });
+
+        }, 5000);
+    */}
   }
+
   render() {
     const instructors = this.state.instructors.map((instructor, index) => (
       <li key={index}>
