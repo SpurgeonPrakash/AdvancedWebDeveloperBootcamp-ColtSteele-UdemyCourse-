28c97c8946ebbabb5225aebd4e5539107f72e801
silvicardo <silvicardo@gmail.com>
2019-03-07 19:11:07 +0100

    todos example backend for  react-redux todolist

---
+++ React/Redux/todos-backend/models/todo.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/todos-backend');
+mongoose.set('debug'. true);
+mongoose.Promise = Promise;//promise Syntax ON
+
+const todoSchema = new mongoose.Schema({task: String});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+module.exports = Todo;
